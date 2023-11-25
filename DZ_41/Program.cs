1031d924979662fed4b044094b4e8adf0a8d6172
olesybabaeva <olesybabaeva@gmail.com>
2023-11-25 10:09:18 -0800

    Решение 41

--- DZ_41/Program.cs
+++ DZ_41/Program.cs
@@ -3,3 +3,26 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
+Console.WriteLine("Введите размер массива: "); 
+// задаем количество чисел в массиве
+int size = Convert.ToInt32(Console.ReadLine()); 
+
+int[] number = new int[size];
+int PositiveNum = 0;
+
+for (int i = 0; i < number.Length; i++)
+{
+    Console.WriteLine($"Введите элемент массива {i}:\t");
+    number[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"Массив: [{String.Join(",", number)}]");
+
+for (int i = 0; i < number.Length; i++)
+{
+    if(number[i] > 0)
+    {
+        PositiveNum++;
+    }
+}
+Console.WriteLine($"Чисел больше 0: {PositiveNum}");
