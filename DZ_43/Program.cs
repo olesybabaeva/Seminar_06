75f747ab0fc2934758056ba6573b87f952159e98
olesybabaeva <olesybabaeva@gmail.com>
2023-11-25 10:34:25 -0800

    Решение задачи 43

--- DZ_43/Program.cs
+++ DZ_43/Program.cs
@@ -4,3 +4,30 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+Console.WriteLine("Введите k1: "); 
+double k1 = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine("Введите k2: "); 
+double k2 = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine("Введите b1: "); 
+double b1 = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine("Введите b2: "); 
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые не пересекаются");
+}
+else if (k1 == k2 && b1 == b2)
+{
+    Console.WriteLine("Прямые совпадают");
+}
+else 
+{
+    Console.WriteLine($"Точка пересечения имеет следующие координаты: ({x};{y})");
+}
